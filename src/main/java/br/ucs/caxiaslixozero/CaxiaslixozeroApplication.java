55ee68d81e7a2db8170df874355c9bbac565a58d
Francisco Fornasier Milani <65914041+FranciscoMilani@users.noreply.github.com>
2024-04-16 19:35:34 -0300

    First commit

---
+++ src/main/java/br/ucs/caxiaslixozero/CaxiaslixozeroApplication.java
@@ -0,0 +1,13 @@
+package br.ucs.caxiaslixozero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaxiaslixozeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaxiaslixozeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ucs/caxiaslixozero/CaxiaslixozeroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucs.caxiaslixozero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaxiaslixozeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
