9e29a7a83397e2a1899427fd1e25f6c755c19aac
Ezequielsb <102973416+Ezequielsb@users.noreply.github.com>
2024-04-29 22:06:15 -0300

    Ajustes formulario ecopontos

--- src/main/java/br/ucs/caxiaslixozero/UI/Ecopoints/Controller/ecopointController.java
+++ src/main/java/br/ucs/caxiaslixozero/UI/Ecopoints/Controller/ecopointController.java
@@ -1,33 +1,57 @@
 package br.ucs.caxiaslixozero.UI.Ecopoints.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.codehaus.groovy.transform.SourceURIASTTransformation;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
+import br.ucs.caxiaslixozero.Domain.Entities.Ecopoint;
 
 @Controller
 public class ecopointController {
 
-    @GetMapping("/registerEcopoint")
-    public ModelAndView registerEcopoint() {
-    	ModelAndView mv = new ModelAndView("registerEcopoint");
-    	String nome = "";
-    	mv.addObject("nome", nome);
-        return mv;
-    }
-    
-    @PostMapping("/saveEcopoint")
-    public ModelAndView saveEcopoint(ModelAndView mv) {
-        mv.addObject("Sucesso");
-        mv.setViewName("registerEcopoint");
-        return mv;
-    }
+	@GetMapping("/registerEcopoint")
+	public ModelAndView registerEcopoint() {
+		ModelAndView mv = new ModelAndView("registerEcopoint");
+		mv.addObject("ecopoint", new Ecopoint());
+		List<String> residuos = new ArrayList<>();
+		residuos.add("Esponjas de cozinha usadas");
+		residuos.add("Embalagens Laminadas (de salgadinho, biscoitos, bombons, etc.)");
+		residuos.add("Raios X (cartelas radiográficas)");
+		residuos.add("Cápsulas de Café (de todas as marcas)");
+		residuos.add("Óleo de Cozinha usado (preferível envasado em garrafa de plástico).");
+		residuos.add("Eletrônicos de pequeno porte (mouse, notebook, celular, carregadores, etc.)");
+		mv.addObject("opcoes", residuos);
+		return mv;
+	}
 
-    @GetMapping("/ecopontos")
-    public String mapaEcopontos() {
-        return "mapainterativo";
-    }
+	@PostMapping("/saveEcopoint")
+	public ModelAndView saveEcopoint(ModelAndView mv, Ecopoint ecopoint,
+			@RequestParam(value = "opcoesSelecionadas", required = false) List<String> opcoesSelecionadas) {
+		System.out.println(ecopoint.getEmail());
+		System.out.println(ecopoint.getCompanyName());
+		System.out.println(ecopoint.getResponsibleName());
+		System.out.println(ecopoint.getResponsiblePhone());
+		System.out.println(ecopoint.getSocialNetwork());
+		System.out.println(ecopoint.getIsPublic());
+		if (opcoesSelecionadas != null) {
+			for (String opcao : opcoesSelecionadas) {
+				System.out.println(opcao);
+			}
+		}
+
+		mv.addObject("Sucesso");
+		mv.setViewName("registerEcopoint");
+		return mv;
+	}
+
+	@GetMapping("/ecopontos")
+	public String mapaEcopontos() {
+		return "mapainterativo";
+	}
 }
